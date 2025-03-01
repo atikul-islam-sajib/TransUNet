29232b732763d3c3aa2b5db9ca1d3162c407f823
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-03-01 14:24:21 +0100

    unzipping is completed

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,14 +2,18 @@ import os
 import sys
 import zipfile
 import torch
+import warnings
 import argparse
 
 sys.path.append("./src/")
+
+from utils import config_files, path_names
 
 
 class Loader:
     def __init__(
         self,
+        image_path=None,
         image_channels: int = 3,
         image_size: int = 224,
         batch_size: int = 4,
@@ -22,8 +26,23 @@ class Loader:
         self.split_size = split_size
         self.shuffle = shuffle
 
+        if image_path is None:
+            self.image_path = path_names()["image_path"]
+        else:
+            warnings.warn(
+                "Ensure the provided image path is correct; an incorrect path may result in an error.".title()
+            )
+            self.image_path = image_path
+
     def unzip_folder(self):
-        pass
+        if os.path.exists(self.image_path):
+            with zipfile.ZipFile(file=self.image_path, mode="r") as zip_file:
+                zip_file.extractall(path=path_names()["processed_data_path"])
+            print(
+                f"Dataset unzipped successfully in {path_names()["processed_data_path"]}".capitalize()
+            )
+        else:
+            raise FileNotFoundError("Image file not found".capitalize())
 
     def split_dataset(self):
         pass
@@ -44,4 +63,12 @@ class Loader:
 
 
 if __name__ == "__main__":
-    pass
+    loader = Loader(
+        image_path="./data/raw/dataset.zip",
+        image_channels=3,
+        image_size=224,
+        batch_size=4,
+        split_size=0.25,
+        shuffle=False,
+    )
+    loader.unzip_folder()
--- src/utils.py
+++ src/utils.py
@@ -49,3 +49,30 @@ def weight_init(m):
         elif classname.find("BatchNorm") != -1:
             nn.init.constant_(m.weight.data, 1.0)
             nn.init.constant_(m.bias.data, 0.0)
+
+
+def path_names():
+    raw_data_path: str = config_files()["artifacts"]["raw_data_path"]
+    processed_data_path: str = config_files()["artifacts"]["processed_data_path"]
+    train_models_path: str = config_files()["artifacts"]["train_models"]
+    best_model_path: str = config_files()["artifacts"]["best_model"]
+    files_path: str = config_files()["artifacts"]["files_path"]
+    metrics_path: str = config_files()["artifacts"]["metrics_path"]
+    train_images: str = config_files()["artifacts"]["train_images"]
+    test_image: str = config_files()["artifacts"]["test_image"]
+    image_path: str = config_files()["dataloader"]["image_path"]
+
+    return {
+        "raw_data_path": raw_data_path,
+        "processed_data_path": processed_data_path,
+        "train_models_path": train_models_path,
+        "best_model_path": best_model_path,
+        "files_path": files_path,
+        "metrics_path": metrics_path,
+        "train_images": train_images,
+        "test_image": test_image,
+        "image_path": image_path,
+    }
+
+
+print(path_names()["image_path"])
