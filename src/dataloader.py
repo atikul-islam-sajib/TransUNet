e956c80379d77bac4f22c2b6a1c7add9e783778d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-03-01 14:05:16 +0100

    dataloader abstract is added

--- src/dataloader.py
+++ src/dataloader.py
@@ -5,3 +5,43 @@ import torch
 import argparse
 
 sys.path.append("./src/")
+
+
+class Loader:
+    def __init__(
+        self,
+        image_channels: int = 3,
+        image_size: int = 224,
+        batch_size: int = 4,
+        split_size: float = 0.25,
+        shuffle: bool = False,
+    ):
+        self.image_channels = image_channels
+        self.image_size = image_size
+        self.batch_size = batch_size
+        self.split_size = split_size
+        self.shuffle = shuffle
+
+    def unzip_folder(self):
+        pass
+
+    def split_dataset(self):
+        pass
+
+    def features_extracted(self):
+        pass
+
+    def create_dataloader(self):
+        pass
+
+    @staticmethod
+    def display_images():
+        pass
+
+    @staticmethod
+    def dataset_details():
+        pass
+
+
+if __name__ == "__main__":
+    pass
