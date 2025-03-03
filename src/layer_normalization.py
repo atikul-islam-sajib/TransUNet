50fba624a8482633d3285b7e604205301a37b518
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2025-03-03 08:51:11 +0100

    layer normalization code is completed

--- src/layer_normalization.py
+++ src/layer_normalization.py
@@ -15,10 +15,10 @@ class LayerNormalization(nn.Module):
         self.layer_norm_eps = layer_norm_eps
 
         self.alpha = nn.Parameter(
-            input=torch.ones((1, 1, self.dimension)), requires_grad=True
+            data=torch.ones((1, 1, self.dimension)), requires_grad=True
         )
         self.beta = nn.Parameter(
-            input=torch.zeros((1, 1, self.dimension)), requires_grad=True
+            data=torch.zeros((1, 1, self.dimension)), requires_grad=True
         )
 
     def forward(self, x: torch.Tensor):
@@ -26,10 +26,16 @@ class LayerNormalization(nn.Module):
             raise ValueError("Input must be a torch.Tensor".capitalize())
         mean = torch.mean(input=x, dim=-1)
         variance = torch.var(input=x, dim=-1)
+
+        mean = mean.unsqueeze(-1)
+        variance = variance.unsqueeze(-1)
+
         normalized_x = (x - mean) / torch.sqrt(variance + self.layer_norm_eps)
 
         return self.alpha * normalized_x + self.beta
 
 
 if __name__ == "__main__":
-    pass
+    layer_norm = LayerNormalization(dimension=512)
+    images = torch.randn((1, 256, 512))
+    print(layer_norm(x=images).size())
