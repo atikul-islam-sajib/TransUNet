099f6d885cec9ad501ebdd7e6a4dd28dd23166d5
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2025-03-04 10:44:22 +0100

    helper code abstract is added

---
+++ src/helper.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import torch
+import argparse
+import torch.nn as nn
+import torch.optim as optim
+
+sys.path.append("./src/")
+
+def load_dataloader():
+    pass
+
+def helper(**kwargs):
+    pass
+
+if __name__ == '__main__':
+    pass
