f5278134bb91a1994ac7177c5f9f7a62f3d9b89d
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2025-03-05 10:17:40 +0100

    tester code is completed

--- src/tester.py
+++ src/tester.py
@@ -7,8 +7,13 @@ import torch.nn as nn
 sys.path.append("./src/")
 
 from transUNet import TransUNet
-from utils import config_files, device_init, plot_images, load_file, path_names
-
+from utils import (
+    config_files,
+    device_init,
+    plot_images,
+    load_file,
+    path_names,
+)
 
 class Tester:
     def __init__(self, dataset: str = "test", device: str = "cuda"):
@@ -34,13 +39,23 @@ class Tester:
         try:
             dataset = self.load_dataloder()
             if self.dataset not in ["train", "test"]:
-                raise ValueError("Invalid dataset. Please choose between 'train' or 'valid'.")
-            
-            dataloader = dataset["train_dataloader"] if self.dataset == "train" else dataset["valid_dataloader"]
-            
-            best_model_path = os.path.join(path_names()["best_model_path"], "best_model.pth")
-            model_state = torch.load(best_model_path, weights_only=False)["model_state_dict"]
-            
+                raise ValueError(
+                    "Invalid dataset. Please choose between 'train' or 'valid'."
+                )
+
+            dataloader = (
+                dataset["train_dataloader"]
+                if self.dataset == "train"
+                else dataset["valid_dataloader"]
+            )
+
+            best_model_path = os.path.join(
+                path_names()["best_model_path"], "best_model.pth"
+            )
+            model_state = torch.load(best_model_path, weights_only=False)[
+                "model_state_dict"
+            ]
+
             model = TransUNet(
                 image_channels=config_files()["dataloader"]["image_channels"],
                 image_size=config_files()["dataloader"]["image_size"],
@@ -52,24 +67,24 @@ class Tester:
                 layer_norm_eps=float(config_files()["TransUNet"]["layer_norm_eps"]),
                 bias=config_files()["TransUNet"]["bias"],
             )
-            
+
             model.to(self.device)
             model.load_state_dict(model_state)
-            
+
             for index, (images, masks) in enumerate(dataloader):
                 original_images = images.to(self.device)
                 original_masks = masks.to(self.device)
                 predicted_images = model(original_images)
-                
+
                 plot_images(
                     original_images=original_images,
                     original_masks=original_masks,
                     predicted_images=predicted_images,
                     predicted=True,
                     epoch=index + 1,
-                    testing=True
+                    testing=True,
                 )
-        
+
         except FileNotFoundError as e:
             print(f"Error: Model file not found - {e}")
         except KeyError as e:
@@ -77,9 +92,26 @@ class Tester:
         except Exception as e:
             print(f"Unexpected error: {e}")
 
+
 if __name__ == "__main__":
-    tester = Tester(
-        dataset="test",
-        device="cuda"
+    parser = argparse.ArgumentParser(description="Test the model.".title())
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default=config_files()["tester"]["dataset"],
+        help="Dataset to test on".capitalize(),
     )
+    parser.add_argument(
+        "--device",
+        type=str,
+        default=config_files()["tester"]["device"],
+        help="Device to use for testing".capitalize(),
+    )
+    args = parser.parse_args()
+
+    dataset = args.dataset
+    device = args.device
+
+    tester = Tester(dataset=dataset, device=device)
+
     tester.display_images()
